879c6677c2f0454d61359850c826f97ca36d9ced
Iain Stewart <iainstewart08@gmail.com>
2022-07-26 23:13:07 +0100

    Basic example of useState

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  const randomTitleText = 'Random title';
+  const [title, setTitle] = useState(randomTitleText);
+
+  const handleClick = () => {
+    title === randomTitleText
+      ? setTitle('Another random title')
+      : setTitle(randomTitleText);
+  };
+
+  return (
+    <>
+      <h2>{title}</h2>
+      <button className='btn' onClick={handleClick}>
+        Change title
+      </button>
+    </>
+  );
 };
 
 export default ErrorExample;
