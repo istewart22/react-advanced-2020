9ac890c0f1f9a2da603e663f21d36de62b69cd06
Iain Stewart <iainstewart08@gmail.com>
2022-07-31 22:55:42 +0100

    Use filter to remove items on click

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name } = person;
+        const handleRemoveItem = (id) => {
+          const filteredPeople = people.filter((person) => person.id != id);
+          setPeople(filteredPeople);
+        };
+        return (
+          <div key={id} className='item'>
+            <h4>{name}</h4>
+            <button onClick={() => handleRemoveItem(id)}>remove</button>
+          </div>
+        );
+      })}
+      <button className='btn' onClick={() => setPeople([])}>
+        Clear all
+      </button>
+    </>
+  );
 };
 
 export default UseStateArray;
